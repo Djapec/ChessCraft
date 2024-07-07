6a770d73b32f43726774129435b522cfa1d0e5cc
Slavko Džulović <dzulovicslavko@gmail.com>
2024-07-07 17:31:30 +0200

    Refactor MosaicView to use MosaicViewBoard instead of AnalysisBoard

--- src/components/mosaic-view/MosaicView.vue
+++ src/components/mosaic-view/MosaicView.vue
@@ -1,37 +1,28 @@
 <template>
   <div class='mosaic-view-container'>
-    <template v-for="gameData in parsedGames">
-      <analysis :parsed-game-data="gameData"></analysis>
-    </template>
+    <mosaic-view-board v-show="parsedGames[0]?.chess" :parsed-game-data="parsedGames[0]"></mosaic-view-board>
+    <mosaic-view-board v-show="parsedGames[1]?.chess" :parsed-game-data="parsedGames[1]"></mosaic-view-board>
+    <mosaic-view-board v-show="parsedGames[2]?.chess" :parsed-game-data="parsedGames[2]"></mosaic-view-board>
+    <mosaic-view-board v-show="parsedGames[3]?.chess" :parsed-game-data="parsedGames[3]"></mosaic-view-board>
   </div>
 </template>
 
 <script>
-  import analysis from "@/components/AnalysisBoard.vue";
+  import MosaicViewBoard from "@/components/mosaic-view/MosaicViewBoard.vue";
   import bus from "@/bus";
 
   export default {
     name: 'MosaicView',
-    components: {
-      analysis,
-    },
-    data() {
+    components: { MosaicViewBoard },
+    data () {
       return {
-        parsedGames: [],
+        parsedGames: [{}, {}, {}, {}],
       }
     },
     created() {
-      bus.$on('loadMosaicView', (items) => {
-        this.loadMosaicView(items)
-      })
-    },
-    methods: {
-      loadMosaicView(parsedDataArray) {
-        this.parsedGames = parsedDataArray;
-        // for (const game of parsedDataArray) {
-        //   bus.$emit('loadGame', game);
-        // }
-      }
+      bus.$on('generateMosaicView', (items) => {
+        this.parsedGames = items;
+      });
     },
   }
 </script>
@@ -41,5 +32,6 @@
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(2, 1fr);
+    grid-gap: 4rem;
   }
 </style>
