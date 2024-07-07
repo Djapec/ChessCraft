b9b8ea0639b771a94f17b29b6c34f915664067df
Slavko Džulović <dzulovicslavko@gmail.com>
2024-07-07 17:29:50 +0200

    Add MosaicViewBoard as a separate component

---
+++ src/components/mosaic-view/MosaicViewBoard.vue
@@ -0,0 +1,41 @@
+<script>
+  import chessBoardCraft from "@/components/chessboard/ChessBoardCraft.vue";
+
+  export default {
+    name: 'MosaicViewBoard',
+    extends: chessBoardCraft,
+    data() {
+      return {
+        viewOnly: true
+      }
+    },
+    props: {
+      parsedGameData: {
+        type: Object,
+      }
+    },
+    watch: {
+      parsedGameData: {
+        immediate: true,
+        handler(newData) {
+          this.loadGame(newData);
+        }
+      }
+    },
+    methods: {
+      loadGame(parsedData) {
+        if (parsedData?.chess) {
+          this.game = parsedData.chess;
+          this.parsedPgnData = parsedData;
+          this.loadPlayers();
+          this.loadPosition();
+        }
+      },
+    },
+    mounted() {
+      this.board.set({
+        viewOnly: true,
+      });
+    },
+  }
+</script>
