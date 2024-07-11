1b5799885402272458bd5e630f8b7bc5de84c4af
Predrag Radovanovic <pedjadjapec197@gmail.com>
2024-07-11 16:32:29 +0200

    Add updateMosaicViewPlayerClock method

--- src/components/mosaic-view/MosaicViewBoard.vue
+++ src/components/mosaic-view/MosaicViewBoard.vue
@@ -1,5 +1,6 @@
 <script>
   import chessBoardCraft from "@/components/chessboard/ChessBoardCraft.vue";
+  import {getInfoForLastTwoMoves} from "../chessboard/Util";
 
   export default {
     name: 'MosaicViewBoard',
@@ -28,7 +29,26 @@
           this.game = parsedData.chess;
           this.parsedPgnData = parsedData;
           this.loadPlayers();
+          this.updateMosaicViewPlayersClock()
           this.loadPosition();
+        }
+      },
+      updateMosaicViewPlayersClock() {
+        if (this.viewOnly) {
+          if (this.parsedPgnData !== null) {
+            const moveDetails = {
+              moveNumber: this.parsedPgnData.halfMoves.length - 1,
+              playedMove: this.parsedPgnData.halfMoves[this.parsedPgnData.halfMoves.length - 1].move
+            }
+            const movesInfo = getInfoForLastTwoMoves(this.parsedPgnData, moveDetails)
+
+            this[movesInfo.currentMoveInfo.color === "white" ? "whitePlayerClock" : "blackPlayerClock"] =
+                movesInfo.currentMoveInfo.clock;
+            if (movesInfo.previousMoveInfo !== null) {
+              this[movesInfo.previousMoveInfo.color === "white" ? "whitePlayerClock" : "blackPlayerClock"] =
+                  movesInfo.previousMoveInfo.clock;
+            }
+          }
         }
       },
     },
