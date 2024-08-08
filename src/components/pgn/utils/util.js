18e03c507bf04c368c8bcf935d97b792f1765401
Predrag Radovanovic <pedjadjapec197@gmail.com>
2024-08-08 21:43:14 +0200

    hotfix

--- src/components/pgn/utils/util.js
+++ src/components/pgn/utils/util.js
@@ -1,3 +1,5 @@
+import { Chess } from "../../../../public/chess.min.js"
+
 export function calculateMoveTime(startTournamentTime, delayInSeconds, cumulativeEMT, emt) {
     const emtSeconds = parseTimeToSeconds(emt || '0:00:00');
     const moveTime = new Date(startTournamentTime.getTime() + (delayInSeconds + cumulativeEMT + emtSeconds) * 1000);
