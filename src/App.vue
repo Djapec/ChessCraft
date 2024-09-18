40049fa4d062bca62ab578cb351a11bbc24c9095
Predrag Radovanovic <pedjadjapec197@gmail.com>
2024-09-18 16:19:56 +0200

    centering content for app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="center-content">
     <router-view></router-view>
   </div>
 </template>
@@ -9,8 +9,19 @@ export default {
   name: 'app'
 }
 </script>
+
 <style>
 body {
   font-family: 'Figtree', sans-serif;
+  margin: 0;
+  height: 100vh;
+}
+
+.center-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
 }
 </style>
